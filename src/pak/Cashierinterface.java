9b6e51a9c13651015ad87d6300bf2af599493445
ashbeyds <delossantos.ashbeybscs2024@gmail.com>
2025-04-13 01:40:25 +0800

    label lang

--- src/pak/Cashierinterface.java
+++ src/pak/Cashierinterface.java
@@ -15,6 +15,8 @@ import java.awt.Canvas;
 import java.awt.Color;
 import javax.swing.JTextField;
 import java.awt.Font;
+import javax.swing.JTextPane;
+import java.awt.Label;
 
 public class Cashierinterface extends JFrame {
 
@@ -22,9 +24,6 @@ public class Cashierinterface extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtEasyStore;
 	private JPanel panel_2;
-	private JTextField txtId;
-	private JTextField txtProduct;
-	private JTextField txtQuantity;
 	private JTextField textField_3;
 	private JTextField textField_4;
 
@@ -49,7 +48,7 @@ public class Cashierinterface extends JFrame {
 	 */
 	public Cashierinterface() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1080, 640);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -61,51 +60,48 @@ public class Cashierinterface extends JFrame {
 		panel.setLayout(null);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(328, 129, 86, 20);
+		textField_3.setBounds(711, 189, 86, 20);
 		panel.add(textField_3);
 		textField_3.setColumns(10);
 		
 		textField_4 = new JTextField();
-		textField_4.setBounds(328, 171, 86, 20);
+		textField_4.setBounds(711, 231, 86, 20);
 		panel.add(textField_4);
 		textField_4.setColumns(10);
 		
 		txtEasyStore = new JTextField();
 		txtEasyStore.setBorder(null);
 		txtEasyStore.setBackground(Color.decode("#ff5050"));
-		txtEasyStore.setFont(new Font("Castellar", Font.PLAIN, 25));
-		txtEasyStore.setText("Easy Store");
-		txtEasyStore.setBounds(10, 11, 168, 42);
+		txtEasyStore.setFont(new Font("Eras Demi ITC", Font.BOLD, 40));
+		txtEasyStore.setText("Easy Sari");
+		txtEasyStore.setBounds(10, 0, 188, 72);
 		panel.add(txtEasyStore);
 		txtEasyStore.setColumns(10);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(null);
 		panel_1.setBackground(Color.decode("#ff5050"));
-		panel_1.setBounds(-97, -31, 595, 103);
+		panel_1.setBounds(-97, -31, 1165, 103);
 		panel.add(panel_1);
 		
 		panel_2 = new JPanel();
 		panel_2.setBackground(Color.LIGHT_GRAY);
-		panel_2.setBounds(309, 0, 115, 359);
+		panel_2.setBounds(751, 0, 303, 591);
 		panel.add(panel_2);
 		
-		txtId = new JTextField();
-		txtId.setText("qwnhqwhuguqgh");
-		txtId.setBounds(0, 83, 86, 20);
-		panel.add(txtId);
-		txtId.setColumns(10);
+		Label label = new Label("Code");
+		label.setFont(new Font("Dialog", Font.BOLD, 19));
+		label.setBounds(10, 78, 54, 22);
+		panel.add(label);
 		
-		txtProduct = new JTextField();
-		txtProduct.setText("Product");
-		txtProduct.setBounds(96, 83, 86, 20);
-		panel.add(txtProduct);
-		txtProduct.setColumns(10);
+		Label label_1 = new Label("Name");
+		label_1.setFont(new Font("Dialog", Font.BOLD, 19));
+		label_1.setBounds(120, 78, 54, 22);
+		panel.add(label_1);
 		
-		txtQuantity = new JTextField();
-		txtQuantity.setText("Quantity");
-		txtQuantity.setBounds(197, 83, 86, 20);
-		panel.add(txtQuantity);
-		txtQuantity.setColumns(10);
+		Label label_1_1 = new Label("Quantity");
+		label_1_1.setFont(new Font("Dialog", Font.BOLD, 19));
+		label_1_1.setBounds(243, 78, 80, 22);
+		panel.add(label_1_1);
 	}
 }
