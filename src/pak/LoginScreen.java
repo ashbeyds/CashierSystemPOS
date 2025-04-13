bd8c931937c539c4686836a838362226585705bf
Sirvyze <zyrusalcaide@gmail.com>
2025-04-13 23:51:25 +0800

    deleted testing file

--- src/pak/LoginScreen.java
+++ src/pak/LoginScreen.java
@@ -8,6 +8,12 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class LoginScreen extends JFrame {
 
@@ -52,8 +58,7 @@ public class LoginScreen extends JFrame {
 		panel.setLayout(null);
 		
 		txtWa = new JTextField();
-		txtWa.setText("wa");
-		txtWa.setBounds(10, 11, 238, 38);
+		txtWa.setBounds(10, 11, 261, 27);
 		panel.add(txtWa);
 		txtWa.setColumns(10);
 		
@@ -64,7 +69,33 @@ public class LoginScreen extends JFrame {
 		panel_1.setLayout(null);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(10, 11, 245, 27);
+		passwordField.setBounds(10, 11, 261, 27);
 		panel_1.add(passwordField);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setBounds(148, 212, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Clear");
+		btnNewButton_1.setBounds(283, 212, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JLabel lblNewLabel = new JLabel("LOGIN");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 17));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(94, 21, 209, 23);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setFont(new Font("Courier New", Font.PLAIN, 15));
+		lblUsername.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsername.setBounds(22, 62, 93, 48);
+		contentPane.add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPassword.setFont(new Font("Courier New", Font.PLAIN, 15));
+		lblPassword.setBounds(22, 139, 93, 48);
+		contentPane.add(lblPassword);
 	}
 }
