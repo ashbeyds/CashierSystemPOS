1aa95b41420965f9f91d885ba0b4e3f670104b94
ashbeyds <delossantos.ashbeybscs2024@gmail.com>
2025-04-13 17:46:57 +0800

    added "add", "delete", and "update" button.
    may table na rin pero di ko pa ayos, kakalikutin ko pa hahaah

--- src/pak/Cashierinterface.java
+++ src/pak/Cashierinterface.java
@@ -17,15 +17,22 @@ import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.JTextPane;
 import java.awt.Label;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
 
 public class Cashierinterface extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField txtEasyStore;
-	private JPanel panel_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
+	private JTable table_code;
+	private JTable table_name;
+	private JTable table_qty;
+	private JTable table_price;
+	private JTable table;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -59,49 +66,75 @@ public class Cashierinterface extends JFrame {
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(711, 189, 86, 20);
-		panel.add(textField_3);
-		textField_3.setColumns(10);
-		
-		textField_4 = new JTextField();
-		textField_4.setBounds(711, 231, 86, 20);
-		panel.add(textField_4);
-		textField_4.setColumns(10);
-		
-		txtEasyStore = new JTextField();
-		txtEasyStore.setBorder(null);
-		txtEasyStore.setBackground(Color.decode("#ff5050"));
-		txtEasyStore.setFont(new Font("Eras Demi ITC", Font.BOLD, 40));
-		txtEasyStore.setText("Easy Sari");
-		txtEasyStore.setBounds(10, 0, 188, 72);
-		panel.add(txtEasyStore);
-		txtEasyStore.setColumns(10);
+		JLabel lblNewLabel = new JLabel("EasySari");
+		lblNewLabel.setFont(new Font("Eras Bold ITC", Font.BOLD, 40));
+		lblNewLabel.setBounds(462, 0, 178, 72);
+		panel.add(lblNewLabel);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(null);
 		panel_1.setBackground(Color.decode("#ff5050"));
-		panel_1.setBounds(-97, -31, 1165, 103);
+		panel_1.setBounds(-97, 0, 1165, 72);
 		panel.add(panel_1);
 		
-		panel_2 = new JPanel();
-		panel_2.setBackground(Color.LIGHT_GRAY);
-		panel_2.setBounds(751, 0, 303, 591);
-		panel.add(panel_2);
+		Label label_code = new Label("Code");
+		label_code.setFont(new Font("Dialog", Font.BOLD, 19));
+		label_code.setBounds(40, 78, 54, 22);
+		panel.add(label_code);
 		
-		Label label = new Label("Code");
-		label.setFont(new Font("Dialog", Font.BOLD, 19));
-		label.setBounds(10, 78, 54, 22);
-		panel.add(label);
+		Label label_name = new Label("Name");
+		label_name.setFont(new Font("Dialog", Font.BOLD, 19));
+		label_name.setBounds(278, 78, 54, 22);
+		panel.add(label_name);
 		
-		Label label_1 = new Label("Name");
-		label_1.setFont(new Font("Dialog", Font.BOLD, 19));
-		label_1.setBounds(120, 78, 54, 22);
-		panel.add(label_1);
+		Label label_qty = new Label("Quantity");
+		label_qty.setFont(new Font("Dialog", Font.BOLD, 19));
+		label_qty.setBounds(512, 78, 80, 22);
+		panel.add(label_qty);
 		
-		Label label_1_1 = new Label("Quantity");
-		label_1_1.setFont(new Font("Dialog", Font.BOLD, 19));
-		label_1_1.setBounds(243, 78, 80, 22);
-		panel.add(label_1_1);
+		Label label_price = new Label("Price");
+		label_price.setFont(new Font("Dialog", Font.BOLD, 19));
+		label_price.setBounds(656, 78, 54, 22);
+		panel.add(label_price);
+		
+		table_code = new JTable();
+		table_code.setBounds(10, 129, 110, 20);
+		panel.add(table_code);
+		
+		table_name = new JTable();
+		table_name.setBounds(130, 130, 357, 20);
+		panel.add(table_name);
+		
+		table_qty = new JTable();
+		table_qty.setBounds(497, 130, 116, 20);
+		panel.add(table_qty);
+		
+		table_price = new JTable();
+		table_price.setBounds(623, 130, 118, 20);
+		panel.add(table_price);
+		
+		table = new JTable();
+		table.setBounds(10, 106, 731, 423);
+		panel.add(table);
+		
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.setBounds(751, 506, 89, 23);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Delete");
+		btnNewButton_1.setBounds(850, 506, 89, 23);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Update");
+		btnNewButton_2.setBounds(949, 506, 89, 23);
+		panel.add(btnNewButton_2);
+		
+		textField = new JTextField();
+		textField.setBounds(751, 130, 100, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		panel.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{lblNewLabel, label_code, label_name, label_qty, label_price, table_code, table_name, table_qty, table_price, table, panel_1, btnNewButton, btnNewButton_1, btnNewButton_2, textField}));
+		
+		
 	}
 }
