a0a6f849c8288f0a684c08e5913613adc6baf09f
Sirvyze <zyrusalcaide@gmail.com>
2025-04-14 13:57:16 +0800

    updaet datbase

--- src/pak/ConnectiontoDATABASE.java
+++ src/pak/ConnectiontoDATABASE.java
@@ -10,7 +10,7 @@ public class ConnectiontoDATABASE {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			 Connection conn = DriverManager.getConnection("jdbc:sqlite:databaselogin.db"); // Copy Qualified name or just copy file location
-			 JOptionPane.showMessageDialog(null, "");
+			 JOptionPane.showMessageDialog(null, "Connected BOI");
 			return conn;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
