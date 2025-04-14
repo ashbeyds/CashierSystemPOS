9dfdff249e4230618189068a189cdd96ab1c3722
ashbeyds <delossantos.ashbeybscs2024@gmail.com>
2025-04-14 20:39:03 +0800

    help me pls pa ayos ng branch

--- src/pak/CashierInterfaceMain.java
+++ src/pak/CashierInterfaceMain.java
@@ -68,7 +68,7 @@ public class CashierInterfaceMain extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Code", "Name", "Quantity", "Price"
             }
         ));
         TableScrollpane.setViewportView(TableContent);
