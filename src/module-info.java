2215334b5834484e351c19bf4bcbf81595db277a
Sirvyze <zyrusalcaide@gmail.com>
2025-04-15 01:04:49 +0800

    ?

--- src/module-info.java
+++ src/module-info.java
@@ -8,15 +8,5 @@ module POSSystem {
 	requires java.desktop;
 	requires java.sql;
     requires org.xerial.sqlitejdbc;
-<<<<<<< Updated upstream
-=======
     requires java.logging;
-    requires org.xerial.sqlitejdbc;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-}
+    }
