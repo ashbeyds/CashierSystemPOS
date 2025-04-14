4714cb727a88996be9debf70c1b7134eb543bcf1
ashbeyds <delossantos.ashbeybscs2024@gmail.com>
2025-04-15 03:39:51 +0800

    Merge branch 'main' of https://github.com/ashbeyds/CashierSystemPOS

--- src/module-info.java
+++ src/module-info.java
@@ -9,4 +9,8 @@ module POSSystem {
 	requires java.sql;
     requires org.xerial.sqlitejdbc;
     requires java.logging;
+<<<<<<< HEAD
     }
+=======
+}
+>>>>>>> 88e33da8930866ff2fea3a0a7221b7e9afd0ea65
